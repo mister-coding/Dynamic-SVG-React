386a40cd9b324b8ce7f4fb028237e94a476a9d16
agik-setiawan <setiawanagik@gmail.com>
2022-09-26 01:00:06 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MySVG from './MySVG';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{width:600}}>
+          <MySVG />
+        </div>
       </header>
     </div>
   );
